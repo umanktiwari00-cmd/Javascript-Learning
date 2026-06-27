2d9f8687ff5e5c7ed1227e5aab7f57ff39e2657b
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-27 14:24:37 +0000

    Data types with operations

--- 01-Basics/02-DataType.js
+++ 01-Basics/02-DataType.js
@@ -1,41 +1,66 @@
-"use strict";    
-// treat javascript all js code as a newer version 
 
-// alert("hello"); use are using node js 
+// // treat javascript all js code as a newer version 
 
-let name = "Umank";
-let age = 32;
-let islogin = true;
-// number 2^53
-// BigInt 
+// // alert("hello"); use are using node js 
 
-// string '' " "
-// Boolean true or false
-// null -> standalone value\ not
-// undefined -> not yet defined
-// symbol 
-// Data Type conversion 
+// let name = "Umank";
+// let age = 32;
+// let islogin = true;
+// // number 2^53
+// // BigInt 
 
-
-
-
-let score   = "33abc";
-
-
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
-
-// number -> 33abc value is nan but type is number 
-//  true is get converted 
-let value = 1;
-let isLoggedIn = Boolean(value);
-console.log(isLoggedIn);
-
-let sumNumber = 33;
-let typeString  = String(sumNumber);
-console.log(typeof(sumNumber));
+// // string '' " "
+// // Boolean true or false
+// // null -> standalone value\ not
+// // undefined -> not yet defined
+// // symbol 
+// // Data Type conversion 
 
 
 
 
+// let score   = "33abc";
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// // number -> 33abc value is nan but type is number 
+// //  true is get converted 
+// let value = 1;
+// let isLoggedIn = Boolean(value);
+// console.log(isLoggedIn);
+
+// let sumNumber = 33;
+// let typeString  = String(sumNumber);
+// console.log(typeof(sumNumber));
+
+// *************** Operations ******************
+
+// let value = 3;
+// let negValue = -value;
+
+// console.log(negValue);
+// let str1 = "hello "
+// let str2 = "Umank";
+// let str3 = str1 + str2 
+// console.log(str3);
+
+
+// console.log("1" + 2); // 12 
+// console.log(1 + "2"); 
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+// console.log(1+2+"2")
+
+
+console.log(+true);
+
+let num1  , num2 ,num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
