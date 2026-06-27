fcca2f155fe5477324e42a119e083e09c344ac50
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-27 14:31:07 +0000

    comparison

---
+++ 01-Basics/04-comparision.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null>=0);
+console.log(null<0);
+console.log(null == 0);
+
+console.log(undefined == 0) // false
+
+//  === strict check use to check the value with the data type too 
