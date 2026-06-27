78ccaf3bfe6ce8c0f3f45ee2165d7247466ecb37
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-27 14:42:13 +0000

    data type summary

---
+++ 01-Basics/data-type-summary.js
@@ -0,0 +1,10 @@
+//  Primitive data type -> call by value
+
+// 7 tpes : String , Number , Boolean , null , undefined ,Symbol(use to make unique),BigInt
+
+//  refrence type --> Non primitive Data type
+// array and objects , Function 
+
+// java script --> Dynamic type lanuage 
+
+
