0f0b2a7c903013909342f32066c42aa57e9ccd5e
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-27 14:08:04 +0000

    conversion of Data Type

---
+++ 01-Basics/02-DataType.js
@@ -0,0 +1,41 @@
+"use strict";    
+// treat javascript all js code as a newer version 
+
+// alert("hello"); use are using node js 
+
+let name = "Umank";
+let age = 32;
+let islogin = true;
+// number 2^53
+// BigInt 
+
+// string '' " "
+// Boolean true or false
+// null -> standalone value\ not
+// undefined -> not yet defined
+// symbol 
+// Data Type conversion 
+
+
+
+
+let score   = "33abc";
+
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// number -> 33abc value is nan but type is number 
+//  true is get converted 
+let value = 1;
+let isLoggedIn = Boolean(value);
+console.log(isLoggedIn);
+
+let sumNumber = 33;
+let typeString  = String(sumNumber);
+console.log(typeof(sumNumber));
+
+
+
+
