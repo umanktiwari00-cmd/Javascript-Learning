00a6bae793c666d249fac3d6aafb991debda7e98
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-27 14:07:43 +0000

    Variables

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,15 @@
+const accountId = 144453
+let accountEmail  = "tiwariumank@gmail.com";
+var accountPassword = "123456"
+accountCity = "Ghazipur"
+
+// accountId = 2; Not  allowed to change the value of cosnt 
+//  don't use var 
+// Prefer not to use because doesn't defined any scope 
+
+let accountState; 
+// Undefined
+console.log(accountId);
+console.log([accountEmail,accountId,accountEmail,accountPassword]);
+console.log(accountState);
+
