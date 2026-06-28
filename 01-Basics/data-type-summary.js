d03f1d0e91b93d09d7b128b84ed816d150a8a4dc
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-06-28 04:52:29 +0000

    heap and stack diffrence

--- 01-Basics/data-type-summary.js
+++ 01-Basics/data-type-summary.js
@@ -16,16 +16,39 @@
 
 // let userEmail;
 
-const heroes = ["shaktiman","naagraj","doga"]
-let myobj= {
-    name : "Umank",
-    age : 22,
+// const heroes = ["shaktiman","naagraj","doga"]
+// let myobj= {
+//     name : "Umank",
+//     age : 22,
 
-};
+// };
 
-const myfunct = function() {
-    console.log("Hello World");
+// const myfunct = function() {
+//     console.log("Hello World");
     
-}
+// }
 
-console.log(typeof heroes);
+// console.log(typeof heroes);
+
+
+//  ******************************* Memory ---> Stack , Heap ****************************
+
+// Stack  ---> Stores into primitive data used the value as a copy
+// Heap ----> Stores into  Non-primitive Data here used as a refrence of the value 
+
+
+ let myYouTubeName = "tiwariumank.com";
+ let anotherName = myYouTubeName
+
+ anotherName = "Chaiorcode";
+ console.log(anotherName);
+ console.log(myYouTubeName);
+ 
+
+ let userone = {
+    name : "Umank"
+ };
+ let usertwo = userone;
+
+ usertwo.name = "Hitesh";
+ console.log(userone);
