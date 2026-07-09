f322a7b7dad8809792920fc31dafba8a5cb65456
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-07-09 06:51:19 +0000

    About scope of a variable

--- 04-Functions/scope.js
+++ 04-Functions/scope.js
@@ -1,8 +1,43 @@
-let a = 300
+// let a = 300
 
-if(true) {
-    let a = 10 
-    const b = 20 
+// if(true) {
+//     let a = 10 
+//     let b = 20 
+// }
+
+// console.log(a);
+
+
+// if(1) {
+//     const uername  = "hitesh" 
+//     if(username ==="hitesh") {
+//         const website = "youtube"
+//         console.log(website);
+        
+//     }
+// }
+
+
+// // 
+
+// function addone(num) {
+//     return num+1;
+// }
+// console.log(addone);
+
+
+// ++++++++++++++++++++++++++++++. interesting ++++++++++++++++++++++++++++++++++
+console.log(addone(5));
+ 
+function addone(num) {
+    return num+1;
 }
 
-console.log(a);
+// console.log(addTwo(5));
+
+
+// const addTwo = function(num) {. In this scenario error is there because we have hold into a variable \
+// //     return num+2;
+// }
+
+
