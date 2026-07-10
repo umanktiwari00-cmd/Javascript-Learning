387b87f174265f09e53cf1925952a35fb6e30c84
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-07-10 05:12:02 +0000

    About loops

---
+++ 05-Control-Flow/Iterator.js
@@ -0,0 +1,32 @@
+// for 
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+let myArray = ["flash","batman","superman"] 
+
+for(let i=0;i<myArray.length;i++) {
+    const element = myArray[i];
+    console.log(element);
+    
+}
+
+// break and continue 
+
+
+// while and do-while
+let t=0;
+while (t<=10) {
+    console.log(t)
+    t+=2
+}
+
+
+do {
+    console.log(1);
+    
+
+}while(1);
