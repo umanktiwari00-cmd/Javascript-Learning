228de9bd6f6e786ea5c3a8428940a5f9f223ac2a
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-07-11 05:53:59 +0000

    About maps

---
+++ 05-Control-Flow/map.js
@@ -0,0 +1,20 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.map((num) => num+10 )
+// console.log(newNums);
+
+// const arr =[]
+
+// const newNums = myNums.forEach((num) => {
+//    arr.push(num+10)
+
+// } )
+
+// console.log(arr);
+
+
+
+
+const newNUms = myNums.map((num) => num*10).map((num) => num+1 ).filter((num) => num>=40)
+
+console.log(newNUms);
