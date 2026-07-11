997f2dfccbba4072f454625641365cc53a549706
umanktiwari00-cmd <umanktiwari00@gmail.com>
2026-07-11 04:58:12 +0000

    return from for each

--- 05-Control-Flow/foreach.js
+++ 05-Control-Flow/foreach.js
@@ -1,37 +1,49 @@
 const coding = ['js','ruby','cpp','Python','java']
 
-// coding.forEach( function (item) {
+// // // coding.forEach( function (item) {
+// // //     console.log(item);
+    
+// // // } )
+
+// coding.forEach( (item) => {
 //     console.log(item);
     
-// } )
+// })
 
-coding.forEach( (item) => {
-    console.log(item);
+
+// // coding.forEach(printMe)
+// // function printMe(item) {
+// //     console.log(item);
     
-})
+// // }
 
-
-// coding.forEach(printMe)
-// function printMe(item) {
+// const myCoding = [
+//     {
+//         languageName : "Javascript",
+//         languageFIleNAme : "Js"
+//     },
+//     {
+//         languageName : "Java",
+//         languageFIleNAme : "JAva"
+//     },
+//     {
+//         languageName : "Python",
+//         languageFIleNAme : "py"
+//     }
+// ]
+// myCoding.forEach((item) => {
 //     console.log(item);
     
-// }
+// })
 
-const myCoding = [
-    {
-        languageName : "Javascript",
-        languageFIleNAme : "Js"
-    },
-    {
-        languageName : "Java",
-        languageFIleNAme : "JAva"
-    },
-    {
-        languageName : "Python",
-        languageFIleNAme : "py"
-    }
-]
-myCoding.forEach((item) => {
-    console.log(item);
+
+// const values = coding.forEach((item) => {
     
-})
+//     console.log(item);
+//     return item
+// })
+// console.log(values); // undefined 
+
+
+
+
